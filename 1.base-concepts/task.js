a598190eeb58f2e32cd4a98fc4192cb3a07056dd
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-09-09 19:23:58 +0300

    Выполнено ДЗ к уроку №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,50 +1,62 @@
-"use strict";
 function solveEquation(a, b, c) {
-  let arr=[];
+    let arr = [];
 
-  let d = (b**2)-(4*a*c);
-  console.log(d)
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
-    let x = -b/(2*a);
-    arr.push(x);
+    let d = (b ** 2) - (4 * a * c);
+    console.log(d)
+    if (d < 0) {
+        arr = [];
+    } else if (d === 0) {
+        let x = -b / (2 * a);
+        arr.push(x);
     } else if (d > 0) {
-        let x1 = (-b + Math.sqrt(d))/(2*a);
-        let x2 = (-b - Math.sqrt(d))/(2*a);
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
         arr.push(x1, x2);
-      };
-  return arr; // array
+    };
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  
 
-  let remainderAmount = (amount - contribution);
-
-  //Считаем количесвто месяцев
-  let endCreditYear = date.getYear();// забираем год
-  let endCreditMonth = date.getMonth(); // забираем месяц
-  let endCreditDay = date.getDate();
-  let nowDate= new Date(); //дата сегодня
-  let nowDateYear = nowDate.getYear();
-  let nowDateMonth = nowDate.getMonth();
-  let nowDateDay = nowDate.getDate();
-
-  let diffMonth;
-
-  if ((nowDateMonth ===endCreditMonth) && (nowDateDay <= endCreditDay)) {
-    diffMonth = (endCreditYear - nowDateYear)*12 + (endCreditMonth - nowDateMonth);
-  } else {
-    diffMonth = (endCreditYear - nowDateYear)*12 + (endCreditMonth - nowDateMonth) - 1;
-  }
-  let monthlyPercent = percent/12/100;
-  let monthPay = remainderAmount*(monthlyPercent + monthlyPercent/(((1 + monthlyPercent)**diffMonth) - 1));
-  let totalAmount = (monthPay * diffMonth).toFixed(2);
+    console.log(percent)
+    let checkForNumberPercent = parseInt(percent);
+    let checkForNumberContribution = parseInt(contribution);
+    let checkForNumberAmount = parseInt(amount);
+    let remainderAmount = amount - contribution;
+    let totalAmount;
 
 
+    if (Number.isNaN(checkForNumberPercent)) {
+        totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
 
-  // код для задачи №2 писать здесь
+    } else if (Number.isNaN(checkForNumberContribution)) {
+        totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    } else if (Number.isNaN(checkForNumberAmount)) {
+        totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    } else {
+        //Считаем количество месяцев
+        let endCreditYear = date.getYear(); // забираем год
+        let endCreditMonth = date.getMonth(); // забираем месяц
+        let endCreditDay = date.getDate(); //забираем день
+        let nowDate = new Date(); //дата сегодня
+        let nowDateYear = nowDate.getYear();
+        let nowDateMonth = nowDate.getMonth();
+        let nowDateDay = nowDate.getDate();
 
-  return totalAmount;
+        let diffMonth; // месяцев до окончания кредита
+
+        if ((nowDateYear === endCreditYear) && (nowDateMonth === endCreditMonth) && (nowDateDay <= endCreditDay)) {
+            diffMonth = (endCreditYear - nowDateYear) * 12 + (endCreditMonth - nowDateMonth) + 1;
+        } else {
+            diffMonth = (endCreditYear - nowDateYear) * 12 + (endCreditMonth - nowDateMonth);
+        };
+
+        let monthlyPercent = percent / 12 / 100;
+        let monthPay = remainderAmount * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** diffMonth) - 1));
+        totalAmount = (monthPay * diffMonth).toFixed(2);
+        console.log(diffMonth);
+        totalAmount = parseFloat(totalAmount);
+
+    }
+    return totalAmount;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    let avg = 0;
 
-  // Ваш код
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        };
 
-  return { min:min, max:max, avg:avg };
+
+        sum += arr[i];
+
+    }
+    avg = ((sum / arr.length).toFixed(2));
+
+    return {
+        min: min,
+        max: max,
+        avg: Number(avg)
+    };
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
+function worker(...arr) {
 
-  // Ваш код
+    let sum1 = 0;
+    let sum2 = 0;
 
-  return sum;
+    for (i = 0; i < arr[0].length; i++) {
+        sum1 += arr[0][i];
+
+    }
+    for (i = 0; i < arr[1].length; i++) {
+        sum2 += arr[1][i];
+    }
+    console.log(sum1 + " " + sum2);
+
+    return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max
+function makeWork(arrOfArr, worker) {
 
-  // Ваш кода
-  // for ...
-  
-  return max
+
+    let max = 0;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
