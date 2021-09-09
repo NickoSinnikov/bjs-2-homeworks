779161f27000b3e86530f347ed33c7d1eabce944
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-09-09 22:39:58 +0300

    ДЗ к второму уроку

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -12,10 +12,7 @@ function getArrayParams(arr) {
         if (arr[i] > max) {
             max = arr[i];
         };
-
-
         sum += arr[i];
-
     }
     avg = ((sum / arr.length).toFixed(2));
 
@@ -26,33 +23,28 @@ function getArrayParams(arr) {
     };
 }
 
+
 // Задание 2
-function worker(...arr) {
-
-    let sum1 = 0;
-    let sum2 = 0;
-
-    for (i = 0; i < arr[0].length; i++) {
-        sum1 += arr[0][i];
-
+function worker(array) {
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
     }
-    for (i = 0; i < arr[1].length; i++) {
-        sum2 += arr[1][i];
-    }
-    console.log(sum1 + " " + sum2);
-
+    console.log("Массив " + array);
+    console.log(sum + " это сумма");
     return sum;
 }
 
-function makeWork(arrOfArr, worker) {
 
+function makeWork(arrOfArr, func) {
 
     let max = 0;
-    // Ваш кода
-    // for ...
+    for (let i = 0; i < arrOfArr.length; i++) {
 
+    }
     return max;
 }
+
 
 // Задание 3
 function worker2(arr) {
