c75ade1e801a7998f7cb243832e6af18c9a572e9
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-09-14 01:11:01 +0300

    Домашнее задание к 3 уроку

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+    let result = arr1.length === arr2.length && arr1.every((item, item2) => item === arr2[item2]);
+    return result;
 
-  return result; // boolean
+    // Ваш код
+
+    return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
-  // Ваш код
+    // Ваш код
 
-  return resultArr; // array
+    return resultArr; // array
 }
