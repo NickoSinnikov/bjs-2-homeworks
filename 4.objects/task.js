a89e94eba35a8cf6258004319a805590c847bdc7
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-09-15 02:10:38 +0300

    Начата задача по уроку №4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    let student1 = new student1("Tony", "male", 37);
+    let student2 = new student2("Buzz", "female", 35);
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+    if (this.mark === undefined) {
+        this.mark = mark;
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+
+Student.prototype.addMark = function(...marks) {
+    for (let i = 0; i < marks.length; i++) {
+        this.mark.push(marks[i]);
+    }
+}
+
+Student.prototype.getAverage = function(average) {
+    for (let i = 0; i < this.mark.length; i++) {
+        let sum = 0;
+        sum += this.mark[i];
+        let average = ((sum / this.mark.length).toFixed(2))
+        this.average = average;
+    }
+}
