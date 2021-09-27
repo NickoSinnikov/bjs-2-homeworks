ea72c2da990844556e0cd2ba2be7b0c15269582f
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-09-28 00:10:28 +0300

    С трудом выполненное задание 1 к уроку №8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,33 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',');
+        console.log("1" + hash);
+        let index = cache.findIndex((item) => !!item[hash]);
+        console.log(index);
+        if (index !== -1) {
+            console.log('Из кэша: ' + cache[index][hash]);
+            return 'Из кэша: ' + cache[index][hash];
+        } else {
+            const result = {};
+            result[hash] = func.call(this, ...args);
+            cache.push(result);
+            if (cache.length > 5) {
+                cache.splice(0, 1);
+            }
+            console.log("Вычисляем: " + result[hash]);
+            return "Вычисляем: " + result[hash];
+        }
+    }
+    return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+    // Ваш код
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
-}
+    // Ваш код
+} // Ваш код
